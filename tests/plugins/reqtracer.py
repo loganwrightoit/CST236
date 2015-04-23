c28c01a4e65484aa0b9289d07aae06b1cb97a336
Mineshopper <kmartshopper@gmail.com>
2015-04-23 01:29:30 -0700

    ReqTracer plugin now outputs results to file.

--- tests/plugins/reqtracer.py
+++ tests/plugins/reqtracer.py
@@ -1,11 +1,15 @@
 from nose2.events import Plugin
-
+from tests.ReqTracer import *
 
 class ReqTracer(Plugin):
     configSection = 'reqtracer'
     commandLineSwitch = ('T', 'reqtracer', 'Turn on requirements tracer')
 
-    def startTestRun(self, event):
-
     def stopTestRun(self, event):
         """Output results to file"""
+        with open('reqtracer-output.txt', 'w') as f:
+            for a in Requirements:
+                f.write("ID: " + a + '\n')
+                f.write("Requirement: " + Requirements[a].req_text)
+                f.write("Functions: " + ", ".join(Requirements[a].func_name))
+                f.write("\n\n")
